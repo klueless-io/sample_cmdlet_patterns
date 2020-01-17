af40dcc8f0323ce731e1a75a5c28a73041abca19
David <david.cruwys@bugcrowd.com>
2020-01-17 16:38:50 +1100

    Initial Commit

---
+++ lib/sample_cmdlet_patterns.rb
@@ -0,0 +1,6 @@
+require "sample_cmdlet_patterns/version"
+
+module SampleCmdletPatterns
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/sample_cmdlet_patterns_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe SampleCmdletPatterns do
+  it "has a version number" do
+    expect(SampleCmdletPatterns::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
