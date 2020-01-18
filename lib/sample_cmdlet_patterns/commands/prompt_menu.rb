a21e0b146536dc4e8fefc3d52f482bf10eb8d976
David <david.cruwys@bugcrowd.com>
2020-01-18 17:31:50 +1100

    esc to exit plus keypress reader command

--- lib/sample_cmdlet_patterns/commands/prompt_menu.rb
+++ lib/sample_cmdlet_patterns/commands/prompt_menu.rb
@@ -9,6 +9,7 @@ module SampleCmdletPatterns
   module Commands
     # menu
     class PromptMenu < SampleCmdletPatterns::Command
+      class ExitApp < StandardError; end
       def initialize(options)
         @options = options
       end
@@ -18,53 +19,63 @@ module SampleCmdletPatterns
       #
       # sample: output.puts 'OK'
       def execute(input: $stdin, output: $stdout)
+
         prompt = TTY::Prompt.new
+
+        prompt.on(:keyctrl_x, :keyescape) do
+          raise ExitApp
+        end
 
         choices = %w[small medium large]
 
-        heading prompt.select('What size? (simple)', choices)
-
-        choices = { small: 1, medium: 2, large: 3 }
-
-        heading prompt.select('What size? (key/value)', choices)
-
-        choices = [
-          { name: 'small', value: 1 },
-          { name: 'medium', value: 2, disabled: '(out of stock)' },
-          { name: 'large', value: 3 }
-        ]
-
-        heading prompt.select('What size? (json config)', choices)
-
-        result = prompt.select('What size? (dsl)') do |menu|
-          menu.choice name: 'small',  value: 1
-          menu.choice name: 'medium', value: 2, disabled: '(out of stock)'
-          menu.choice name: 'large',  value: 3
+        begin
+          heading prompt.select('What size? (simple)', choices)
+        rescue ExitApp
+          puts
+          prompt.warn 'e[x]iting....'
         end
 
-        heading result
+        # choices = { small: 1, medium: 2, large: 3 }
 
-        result = prompt.select('Choose your destiny (custom value via proc)?') do |menu|
-          menu.choice 'Scorpion', 1
-          menu.choice 'Kano', 2
-          menu.choice 'Jax', -> { 'Nice choice captain!' }
-        end
+        # heading prompt.select('What size? (key/value)', choices)
 
-        heading result
+        # choices = [
+        #   { name: 'small', value: 1 },
+        #   { name: 'medium', value: 2, disabled: '(out of stock)' },
+        #   { name: 'large', value: 3 }
+        # ]
 
-        result = prompt.select('Choose your destiny (default)?') do |menu|
-          menu.default 3
+        # heading prompt.select('What size? (json config)', choices)
 
-          menu.choice 'Scorpion', 1
-          menu.choice 'Kano', 2
-          menu.choice 'Jax', 3
-        end
+        # result = prompt.select('What size? (dsl)') do |menu|
+        #   menu.choice name: 'small',  value: 1
+        #   menu.choice name: 'medium', value: 2, disabled: '(out of stock)'
+        #   menu.choice name: 'large',  value: 3
+        # end
 
-        heading result
+        # heading result
 
-        heading prompt.select('Choose your destiny (cycle)?', %w(Scorpion Kano Jax), cycle: true)
+        # result = prompt.select('Choose your destiny (custom value via proc)?') do |menu|
+        #   menu.choice 'Scorpion', 1
+        #   menu.choice 'Kano', 2
+        #   menu.choice 'Jax', -> { 'Nice choice captain!' }
+        # end
 
-        prompt.select('Choose your destiny (extra config)?', %w(Scorpion Kano Jax), help: '(Bash keyboard)', symbols: {marker: '>'})
+        # heading result
+
+        # result = prompt.select('Choose your destiny (default)?') do |menu|
+        #   menu.default 3
+
+        #   menu.choice 'Scorpion', 1
+        #   menu.choice 'Kano', 2
+        #   menu.choice 'Jax', 3
+        # end
+
+        # heading result
+
+        # heading prompt.select('Choose your destiny (cycle)?', %w(Scorpion Kano Jax), cycle: true)
+
+        # prompt.select('Choose your destiny (extra config)?', %w(Scorpion Kano Jax), help: '(Bash keyboard)', symbols: {marker: '>'})
 
         :gui
       end
--- lib/sample_cmdlet_patterns/exit_app.rb
+++ lib/sample_cmdlet_patterns/exit_app.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module SampleCmdletPatterns
-  VERSION = '0.0.14'
+  class ExitApp < StandardError; end
 end
